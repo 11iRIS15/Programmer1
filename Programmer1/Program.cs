678efbf19bce2716af498552cc7be20ccfb0bc0b
Iris Atanante <atananteiris0@gmail.com>
2025-07-25 18:35:49 +0800

    Initial Commit

--- Programmer1/Program.cs
+++ Programmer1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!.....");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your first name: "); //asked the user
+        string firstName = Console.ReadLine();
+
+        Console.Write("Enter your last name: "); //asked the user
+        string lastName = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName)) //check if the name is empty
+        {
+            Console.WriteLine("Error: First name and last name cannot be empty."); //display if error
+        }
+        else
+        {
+            Console.WriteLine($"Your full name is: {firstName} {lastName}"); //display if it is successful
+        }
+    }
+}
